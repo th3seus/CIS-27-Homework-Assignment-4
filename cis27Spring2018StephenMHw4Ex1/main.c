f146a509bd090914f3eb625baeec85ec686278d3
Stephen <macbook@macbooks-macbook-pro-3.local>
2018-03-19 16:36:40 -0700

    Initial Commit

---
+++ cis27Spring2018StephenMHw4Ex1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cis27Spring2018StephenMHw4Ex1
+//
+//  Created by Stephen on 3/19/18.
+//  Copyright © 2018 Stephen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
